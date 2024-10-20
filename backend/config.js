6083df7ea6aabd6914be642404973d711a58baae
Chaitanya <chaitanyaasole@gmail.com>
2024-10-20 14:50:58 +0530

    added .env

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  "mongodb+srv://root:root@book-store-mern.ub1g3.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BOOK-STORE-MERN";
