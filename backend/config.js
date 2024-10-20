fedbd46ba770ade5831b544410f8f8fff36d1990
Chaitanya <chaitanyaasole@gmail.com>
2024-10-20 09:11:08 +0530

    Made Book Store

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@book-store-mern.ub1g3.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BOOK-STORE-MERN";
